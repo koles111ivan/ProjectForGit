ca38012a0d60bc171c192595806a717346af8b0a
Ivan <ivan.kolesnichenko.91@inbox.ru>
2025-03-31 14:55:56 +0300

    Убрал все лишнее

--- git_lol/Program.cs
+++ git_lol/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//ertreieqgjegeg
-// lol
-//ioioioioioioioioi
-//lolololololol
-//efefefef
-//tit
